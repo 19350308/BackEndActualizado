a2b7368a4561d96093cb1163d8d79e90f01321c1
19350308 <isc19350308@gmail.com>
2022-09-28 10:12:10 -0500

    ERRORES

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,15 +1,15 @@
 const {request, response} = require ("express")
 
-const rootMessage = (req=request, res=response) =>{res.json({msg:"Mensajes"})}
+const rootMessage = (req=request, res=response) =>{res.status(400).json({msg:"Mensajes"})}
 
-const hiMessage = (req=request, res=response) =>{res.json({msg:"Hola Mundo"})}
+const hiMessage = (req=request, res=response) =>{res.status(401).json({msg:"Hola Mundo"})}
 
-const byeMessage = (req=request, res=response) =>{res.json({msg:"Adios Mundo"})}
+const byeMessage = (req=request, res=response) =>{res.status(402).json({msg:"Adios Mundo"})}
 
-const postMessage = (req=request, res=response) =>{res.json({msg:"Mensajes Post"})}
+const postMessage = (req=request, res=response) =>{res.status(403).json({msg:"Mensajes Post"})}
 
-const putMessage = (req=request, res=response) =>{res.json({msg:"Mensajes PUT"})}
+const putMessage = (req=request, res=response) =>{res.status(404).json({msg:"Mensajes PUT"})}
 
-const deleteMessage = (req=request, res=response) =>{res.json({msg:"Mensajes Delete"})}
+const deleteMessage = (req=request, res=response) =>{res.status(405).json({msg:"Mensajes Delete"})}
 
 module.exports = {rootMessage, hiMessage, byeMessage, postMessage, putMessage, deleteMessage}
